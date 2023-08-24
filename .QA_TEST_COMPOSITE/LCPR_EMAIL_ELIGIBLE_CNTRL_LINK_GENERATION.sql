060532f265207c3b39360af7ec7246d107f39a4d
IsabellaGV99 <141296799+IsabellaGV99@users.noreply.github.com>
2023-08-24 18:48:50 -0500

    Update LCPR_EMAIL_ELIGIBLE_CNTRL_LINK_GENERATION.sql

--- .QA_TEST_COMPOSITE/LCPR_EMAIL_ELIGIBLE_CNTRL_LINK_GENERATION.sql
+++ .QA_TEST_COMPOSITE/LCPR_EMAIL_ELIGIBLE_CNTRL_LINK_GENERATION.sql
@@ -1,4 +1,3 @@
-WITH CROSS_ELIGIBLE AS(
 with
 csr_attributes as (
     SELECT 
@@ -47,9 +46,13 @@ from "prod"."public"."flagging"
 ),
 
 RS_view_attributes as (
-     SELECT 
-         *
-     FROM "prod"."public"."lcpr_customer_service_features"
+    SELECT 
+        account_id AS numero,
+        lst_bill_dt, 
+        CAST(DATEADD(SECOND, lst_bill_dt/1000,'1970/1/1') AS DATE) AS lst_bill_date, 
+        current_date-0 AS period_evaluated,
+        *
+    FROM "prod"."public"."lcpr_customer_service_features"
  ),
 
  offers as (
@@ -113,161 +116,7 @@ source_qualify as (
         -- condición de flagging
         (open_order = false  or open_order is null) and
         (trouble_call = false  or trouble_call is null) 
-)
-SELECT
-        DISTINCT source_qualify.numero_cuenta
-FROM source_qualify left join retargeting_suppres on source_qualify.numero_cuenta = retargeting_suppres.retargeting_Account_id
-where retargeting_suppres.retargeting_Account_id is null)
-,
-
--- SINGLE OFFER ELIGIBLE
-
-SINGLE_OFFER_ELIGIBLE AS(
-    WITH
-csr_attributes as(
-    SELECT 
-        account_id as numero1
-    from  "prod"."public"."insights_customer_services_rates_lcpr"
-    WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
 ),
-flagging_attributes as (
-select
-    account_id,
-    case when has_privacy_flag = 'X' then true else false end as privacy_flag 
-from "prod"."public"."flagging"
-),
-offers as (
-    SELECT 
-        account_id AS num_id,
-        offer_type,
-        rank,
-        next_best_action_date
-    FROM "prod"."public"."lcpr_offers"
-)
-select 
-    distinct CSR_FLAGG.numero1
-FROM 
-(SELECT 
-    *
-FROM csr_attributes LEFT JOIN flagging_attributes 
-ON csr_attributes.numero1 = flagging_attributes.account_id) CSR_FLAGG LEFT JOIN offers ON CSR_FLAGG.numero1 = offers.num_id
-WHERE 
-    --condiciones offers
-    lower(offers.offer_type) = 'single' and 
-    offers.rank = 1 and 
-    next_best_action_date= current_date AND
-    
-    --condiciones flagging
-    (CSR_FLAGG.privacy_flag = false or CSR_FLAGG.privacy_flag is null)
-
-)
-,
-
---SINGLE OFFER EMAIL ELIGIBLE
-
-SINGLE_OFFER_EMAIL_ELIGIBLE AS(
-
-WITH
-csr_attributes as(
-    SELECT 
-        account_id as cuenta,
-        *
-    from  "prod"."public"."insights_customer_services_rates_lcpr"
-    WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
-),
-offers as (
-    SELECT 
-        account_id AS num_id,
-        *
-    FROM "prod"."public"."lcpr_offers"
--- limit 100
-),
-cs_features as (
-    SELECT 
-        account_id AS numero,
-        lst_bill_dt, 
-        CAST(DATEADD(SECOND, lst_bill_dt/1000,'1970/1/1') AS DATE) AS converted_date, 
-        current_date-10 AS period_evaluated
-    FROM "prod"."public"."lcpr_customer_service_features"
-)
-select 
-    distinct cuenta
-FROM 
-(SELECT 
-    *
-FROM csr_attributes LEFT JOIN offers 
-ON csr_attributes.cuenta = offers.num_id) CSR_OFFERS LEFT JOIN cs_features ON csr_offers.cuenta = cs_features.numero
-WHERE 
-    hsd=1 and
-    channel = 'email' and 
-    email is not null  and 
-    converted_date <= period_evaluated
-
-),
-
--- CONTROL CUSTOMER
-
-CONTROL_CUST AS(
-
-with 
-csr_customers AS (
-    SELECT 
-        account_id as cuenta1,
-        as_of as csr_dte,
-        CASE WHEN welcome_offer = 'X' THEN true ELSE false END as welcome_offer,
-        CASE WHEN acp = 'X' THEN true ELSE false END as subsidize_flag,
-        CASE WHEN joint_customer = 'X' THEN true ELSE false END as joint_customer,
-        CASE 
-            WHEN  
-                substring(bill_code ,1,1) IN ('R','F') AND 
-                substring(bill_code ,2,1) IN ('1','2','3','4','5','6','7') 
-                THEN true ELSE false END    as valid_pckg,
-        CASE 
-            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('24', '99', '67', '41', '19') then 'Control A'
-            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('25', '47', '79', '22', '93') then 'Control X' 
-            WHEN  substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)   )-1,2) in ('26', '48', '80', '23', '94') then 'Control J' ELSE 'Target'
-        END as regime,
-        addr1_hse as CUST_ADDR1,
-        home_phone_sbb as PHONE_1,
-        bus_phone_sbb as PHONE_2,
-        cyc_cde_sbb as INVOICE_DAY,
-        email as email_csr,
-        cust_typ_sbb AS CUST_TYPE,
-        delinquency_days AS DELINQUENCY_DAYS,
-        hsd AS hsd_csr,
-        bill_code AS PCK_CODE,
-        res_name_sbb AS CUST_NAME,
-        *
-    FROM "prod"."public"."insights_customer_services_rates_lcpr"
-    WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
-),
-offers as (
-    SELECT 
-        account_id,
-        max(regime) as regime_of
-    FROM "prod"."public"."lcpr_offers"
-    group by 1
-)
-SELECT 
-    Distinct csr_customers.cuenta1
-FROM csr_customers LEFT JOIN offers ON csr_customers.cuenta1 = offers.account_id
-WHERE lower(offers.regime_of) like ('%control%')
-
-)
-,
-
--- EMAIL RETARGETING
-EMAIL_RET AS(
-
-WITH csr_attributes as ( 
-SELECT  
-account_id as account, 
-as_of as csr_dte 
-from "prod"."public"."insights_customer_services_rates_lcpr" 
-WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr"))
-
-, 
-
 LAST_COMMS_42 AS ( 
     SELECT  
         account_id,
@@ -277,7 +126,6 @@ LAST_COMMS_42 AS (
         CASE WHEN date >= period then true else false end as condition
     FROM 
          "prod"."public"."lcpr_last_comms")
-
 
 ,
 LAST_COMMS_1M AS ( 
@@ -294,7 +142,6 @@ FROM "prod"."public"."lcpr_last_comms")
 
 ,
 
-
 LAST_COMMS_21 AS ( 
     SELECT
         account_id,
@@ -304,15 +151,32 @@ LAST_COMMS_21 AS (
         CASE WHEN date >= period then true else false end as condition
     FROM 
          "prod"."public"."lcpr_last_comms"
-) 
+),
 
+TO_AWS AS (
+    SELECT
+        account_id AS numero,
+        CASE WHEN lower(ord_typ) IN ('downgrade', 'upgrade', 'sidegrade') THEN ls_chg_dte_ocr ELSE null END AS migration_dte,
+        ord_typ
+    FROM 
+        "prod"."public"."transactions_orderactivity"
+),
 
+CROSS_ELIGIBLE as 
+(
+    SELECT
+        source_qualify.numero_cuenta
+    FROM source_qualify left join retargeting_suppres on source_qualify.numero_cuenta = retargeting_suppres.retargeting_Account_id
+    where retargeting_suppres.retargeting_Account_id is null
+),
+
+EMAIL_RETARGETING as (
 SELECT
-DISTINCT a.account
+    a.numero_cuenta
 From csr_attributes a
-Left JOIN LAST_COMMS_42 b ON a.account = b.account_id
-Left JOIN LAST_COMMS_1M c ON a.account = c.account_id
-Left JOIN LAST_COMMS_21 d ON a.account = d.account_id
+Left JOIN LAST_COMMS_42 b ON a.numero_cuenta = b.account_id
+Left JOIN LAST_COMMS_1M c ON a.numero_cuenta = c.account_id
+Left JOIN LAST_COMMS_21 d ON a.numero_cuenta = d.account_id
 Where 
     -- out_call_sent_6_weeks
     b.condition = true  
@@ -322,69 +186,61 @@ Where
     or  
     -- -- email sent v3 weeks
     d.condition =true
-
-)
-,
-
--- SIDEGRADE RETARGETING
-
-SIDEGRADE_RET AS(
-
-with
-csr_attributes as (
-    SELECT 
-        account_id as account_id_num,
-        sub_acct_no_sbb as cust_acct,
-        as_of as csr_dte,
-        CASE WHEN welcome_offer = 'X' THEN true ELSE false END as welcome_off,
-        CASE WHEN acp = 'X' THEN true ELSE false END as subsidize_fl,
-        CASE WHEN joint_customer = 'X' THEN true ELSE false END as joint_cust,
-        CASE 
-            WHEN  
-                substring(bill_code ,1,1) IN ('R','F') AND 
-                substring(bill_code ,2,1) IN ('1','2','3','4','5','6','7') 
-                THEN true ELSE false END    as valid_pckg,
-        CASE 
-            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('24', '99', '67', '41', '19') then 'Control A'
-            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('25', '47', '79', '22', '93') then 'Control X' 
-            WHEN  substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)   )-1,2) in ('26', '48', '80', '23', '94') then 'Control J' ELSE 'Target'
-        END as regime,
-        addr1_hse as CUST_ADDR1,
-        home_phone_sbb as PHONE_1,
-        bus_phone_sbb as PHONE_2,
-        cyc_cde_sbb as INVOICE_DAY,
-        email as EMAIL,
-        cust_typ_sbb AS CUST_TYPE,
-        delinquency_days AS DELINQUENCY_DAYS,
-        hsd AS HSD,
-        bill_code AS PCK_CODE,
-        res_name_sbb AS CUST_NAME
-            from "prod"."public"."insights_customer_services_rates_lcpr"
-   WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
-    -- limit 100
 ),
-TO_AWS AS (
-    SELECT
-        account_id AS numero,
-        CASE WHEN lower(ord_typ) IN ('downgrade', 'upgrade', 'sidegrade') THEN ls_chg_dte_ocr ELSE null END AS migration_dte,
-        ord_typ
-    FROM 
-        "prod"."public"."transactions_orderactivity"
-)
+
+SIDEGRADE_RETARGETING as 
+(
 select 
-   distinct account_id_num
-from csr_attributes CSR left join TO_AWS TORDER on CSR.account_id_num = TORDER.numero
+   numero_cuenta
+from csr_attributes CSR left join TO_AWS TORDER on CSR.numero_cuenta = TORDER.numero
 WHERE 
      migration_dte >= DATEADD(month, -6, CURRENT_DATE)
+),
 
+SINGLE_OFFER_EMAIL_ELIGIBLE as ( 
+select 
+    csr_attributes.numero_cuenta
+FROM csr_attributes LEFT JOIN offers ON csr_attributes.numero_cuenta = offers.account_id
+LEFT JOIN RS_view_attributes ON csr_attributes.numero_cuenta = RS_view_attributes.account_id
+WHERE 
+    hsd=1 and
+    channel = 'email' and 
+    email is not null  and 
+    lst_bill_date <= period_evaluated
+),
+
+SINGLE_OFFER_ELIGIBLE as (
+select 
+    CSR_FLAGG.numero_cuenta
+FROM 
+(SELECT 
+    *
+FROM csr_attributes LEFT JOIN flagging_attributes 
+ON csr_attributes.numero_cuenta = flagging_attributes.account_id) CSR_FLAGG LEFT JOIN offers ON CSR_FLAGG.numero_cuenta = offers.account_id
+WHERE 
+    --condiciones offers
+    lower(offers.offer_type) = 'single' and 
+    offers.rank = 1 and 
+    next_best_action_date= current_date AND
+    
+    --condiciones flagging
+    (CSR_FLAGG.privacy_flag = false or CSR_FLAGG.privacy_flag is null)
+),
+
+CONTROL_CUST AS (
+    SELECT 
+        csr_attributes.numero_cuenta
+    FROM csr_attributes LEFT JOIN offers ON csr_attributes.numero_cuenta = offers.account_id 
+    WHERE lower(offers.regime) like ('%control%')
 )
 
 SELECT
-COUNT(DISTINCT a.numero_cuenta)
-From CROSS_ELIGIBLE a
-INNER JOIN SINGLE_OFFER_ELIGIBLE b ON a.numero_cuenta = b.numero1
-FULL OUTER JOIN SINGLE_OFFER_EMAIL_ELIGIBLE c ON a.numero_cuenta = c.cuenta
-FULL OUTER JOIN CONTROL_CUST d ON a.numero_cuenta = d.cuenta1
-Left JOIN EMAIL_RET e ON a.numero_cuenta = e.account 
-Left JOIN SIDEGRADE_RET f ON a.numero_cuenta = f.account_id_num 
-WHERE e.account is null and f.account_id_num is null
+    COUNT(DISTINCT csr_attributes.numero_cuenta)
+From csr_attributes
+    INNER JOIN CROSS_ELIGIBLE ON csr_attributes.numero_cuenta = CROSS_ELIGIBLE.numero_cuenta
+    INNER JOIN SINGLE_OFFER_ELIGIBLE  ON csr_attributes.numero_cuenta = SINGLE_OFFER_ELIGIBLE.numero_cuenta
+    INNER JOIN SINGLE_OFFER_EMAIL_ELIGIBLE ON csr_attributes.numero_cuenta = SINGLE_OFFER_EMAIL_ELIGIBLE.numero_cuenta
+    INNER JOIN CONTROL_CUST  ON csr_attributes.numero_cuenta = CONTROL_CUST.numero_cuenta
+    Left JOIN EMAIL_RETARGETING ON csr_attributes.numero_cuenta = EMAIL_RETARGETING.numero_cuenta 
+    Left JOIN SIDEGRADE_RETARGETING ON csr_attributes.numero_cuenta = SIDEGRADE_RETARGETING.numero_cuenta 
+    WHERE EMAIL_RETARGETING.numero_cuenta is null and SIDEGRADE_RETARGETING.numero_cuenta is null
