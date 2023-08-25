c42dcea73aab11e044a3a1124a04bf5267303d20
IsabellaGV99 <141296799+IsabellaGV99@users.noreply.github.com>
2023-08-25 09:22:59 -0500

    Update LCPR_SINGLE_OFFER_ELIGIBLE.sql

--- .QA_TEST/LCPR_SINGLE_OFFER_ELIGIBLE.sql
+++ .QA_TEST/LCPR_SINGLE_OFFER_ELIGIBLE.sql
@@ -1,3 +1,4 @@
+-- LCPR_SINGLE_OFFER_ELIGIBLE Redshift
 WITH
 csr_attributes as(
     SELECT 
@@ -5,32 +6,39 @@ csr_attributes as(
     from  "prod"."public"."insights_customer_services_rates_lcpr"
     WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
 ),
+
 flagging_attributes as (
-select
-    account_id,
-    case when has_privacy_flag = 'X' then true else false end as privacy_flag 
+select 
+    *,
+    sub_acct_no_sbb as cust_acct,
+    case when has_privacy_flag = 'X' then true else false end as privacy_flag ,
+    case when has_open_order  = 'X' then true else false end as open_order ,
+    case when pending_tc  = 'X' then true else false end as trouble_call ,
+    case when is_in_ndnc  = 'X' then true else false end as dnt_call_flag
 from "prod"."public"."flagging"
+-- limit 100
 ),
 offers as (
     SELECT 
         account_id AS num_id,
-        offer_type,
-        rank,
-        next_best_action_date
+        *
     FROM "prod"."public"."lcpr_offers"
+
 )
+
 select 
     count(distinct CSR_FLAGG.numero_cuenta)
 FROM 
 (SELECT 
     *
 FROM csr_attributes LEFT JOIN flagging_attributes 
-ON csr_attributes.numero_cuenta = flagging_attributes.account_id) CSR_FLAGG LEFT JOIN offers ON CSR_FLAGG.numero_cuenta = offers.num_id
+ON csr_attributes.numero_cuenta = flagging_attributes.account_id) CSR_FLAGG LEFT JOIN offers ON CSR_FLAGG.numero_cuenta = offers.account_id
 WHERE 
     --condiciones offers
+    -- offers.next_best_action_date = and 
     lower(offers.offer_type) = 'single' and 
     offers.rank = 1 and 
-    next_best_action_date= current_date AND
+
     
     --condiciones flagging
-    (CSR_FLAGG.privacy_flag = false or CSR_FLAGG.privacy_flag is null)
+    CSR_FLAGG.privacy_flag = false -- and
